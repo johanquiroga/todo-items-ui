6b4d8197acf33596fa1b8a3e7fa558626a9d48d8
Johan Quiroga <quirogacj@utp.edu.co>
2018-04-29 23:07:11 -0500

    Modified Todo component so it shows all the relevant information about each task. Added some configuration constants

--- src/components/Todo/index.js
+++ src/components/Todo/index.js
@@ -1,14 +1,22 @@
 import React from 'react';
-import { List } from 'semantic-ui-react';
+import { List, Label } from 'semantic-ui-react';
 import { withState, compose } from 'recompose';
+import 'react-dates/initialize';
+import 'react-dates/lib/css/_datepicker.css';
+import { SingleDatePicker } from 'react-dates';
+import moment from 'moment';
+import { priorities } from '../../constants';
 
 const Todo = ({
   onTodoClick,
   onDeleteClick,
   completed,
   name,
+	dueDate,
   isFocused,
-  setFocus
+  setFocus,
+	priority,
+	focused
 }) => (
     <List.Item
       onDoubleClick={() => console.log('double click')}
@@ -45,11 +53,24 @@ const Todo = ({
           'none'
         }}
       >
-        {name}
+	      {name} - <small>Due date:
+		      <SingleDatePicker
+			      date={moment(dueDate)}
+			      onDateChange={newDate => console.log(newDate)}
+			      focused={focused}
+			      onFocusChange={() => console.log()}
+			      small
+			      noBorder
+			      numberOfMonths={1}
+			      readOnly
+		      />
+        </small>
+	      <Label as='span' color={!completed ? priorities[priority].color : 'grey'} tag>{priorities[priority].text}</Label>
       </List.Content>
     </List.Item>
 );
 
 export default compose(
   withState('isFocused', 'setFocus', false),
+	withState('focused', 'setFocused', false),
 )(Todo);
--- src/constants/index.js
+++ src/constants/index.js
@@ -6,3 +6,9 @@ export const errorCondition = (props) =>
 
 export const authCondition = (props) =>
 	props.isAuth;
+
+export const priorities = {
+	0: {text: 'Low', color: 'green'},
+	1: {text: 'Normal', color: 'orange'},
+	2: {text: 'High', color: 'red'},
+};
